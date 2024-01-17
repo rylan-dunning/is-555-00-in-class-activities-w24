a64f7ad528748743c2b49b0d949ce2f954932ff5
rylan-dunning <brdunning14@gmail.com>
2024-01-17 14:34:54 -0700

    Update data wrangling 1 - Section 3.R

--- data wrangling 1 - Section 3.R
+++ data wrangling 1 - Section 3.R
@@ -5,6 +5,8 @@ library(tidyverse)
 # with it.
 df <- starwars
 
+df
+
 # glimpse turns a data frame on its side. Super useful.
 df %>%
   glimpse()
@@ -18,14 +20,35 @@ df %>%
 # arrange by mass
 # note: filtering on some logical excludes NAs
 
+answer_obj_name <- df %>% 
+  filter(height > 100,
+         sex == 'female') %>% 
+  select(name, height, mass, species, films) %>% 
+  filter(mass > 50) %>% 
+  arrange(mass)
+
+
+df
 
 # calculate a new column,weight_lbs = mass * 2.204623
 # Make sure it gets saved to the tibble...
 
+df <- df %>% 
+  mutate(weight_lbs = mass * 2.204623)
 
 
 # group and summarize. Pay attention to NAs
 # get a count and mean mass by species
+
+df %>% glimpse
+
+df %>% 
+  group_by(species) %>% 
+  summarize(species_count = n(),
+            avg_mass = mean(mass, na.rm = TRUE))
+
+df %>% 
+  filter(species =='Droid')
 
 
 # Lots of NAs for average... why? Even for large groups it's NA...
@@ -43,13 +66,21 @@ df %>%
 
 # top 5 tallest overall - using slice_head
 
+df %>% 
+  arrange(desc(height)) %>% 
+  slice_head(n = 5)
+
+df %>% 
+  slice_max(mass, n = 5)
 
 # or just using slice_max
 
 
 # what is the shortest character for each species? 
 
-
+df %>% 
+  group_by(species) %>% 
+  slice_max(height, with_ties = F)
 
 
 # ALso summarize isn't always the goal what if we want a relative 
